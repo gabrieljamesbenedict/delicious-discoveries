5adf19284bb0dc1c9e68f50968ac668b8b7322fb
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-01 02:39:59 +0800

    feat: add Header tag to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from './Component/Header';
 import logo from './logo.svg';
 import './Styles/App.css';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Header />
+      <h1>Hello world</h1>
+    </>
   );
 }
 
---
+++ src/Component/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            <h1> Delicious Discoveries</h1>
+        </div>
+    );
+}
+
+export default Header;

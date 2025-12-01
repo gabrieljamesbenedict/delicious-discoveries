fcc5634d6d484b39c515186010d6ae41c468afeb
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-02 05:36:36 +0800

    feat: add Home page layout and styles for banner and featured delicacies

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,38 @@
+import "../Styles/Home.css";
+
 function Home() {
+
+    const bannerTitle = `Discover the Hidden Treasures of Marinduqueño Cuisine`;
+    const bannerText = `
+    Famously known as the Heart of the Philippines, Marinduque 
+    offers a rich and unique selection of traditional dishes that reflect its vibrant 
+    culture and history. From savory specialties to sweet treats, explore the flavors 
+    that make Marinduqueño cuisine a true delight for food enthusiasts.
+    `;
+
     return (
         <>
-            <p>Home Page</p>
+            <section className="banner">
+                <div className="banner-content">
+                    <h1 className="banner-title">{bannerTitle}</h1>
+                    <p className="banner-text">{bannerText}</p>
+                    <button className="banner-button">Discover More</button>
+                </div>
+            </section>
+
+
+            <section className="featured-delicacies">
+                <div className="featured-delicacies-content">
+                    <h1 className="featured-delicacies-title">
+
+                    </h1>
+                </div>
+            </section>
+
+
+            <section className="cultural-highlights">
+
+            </section>
         </>
     );
 }

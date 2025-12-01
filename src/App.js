83565f49705c70a00e43c2b4e11fbe21e1fca253
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-02 01:51:36 +0800

    feat: add nav buttons to header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Header from './Component/Header';
-import logo from './logo.svg';
 import './Styles/App.css';
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello world</h1>
     </>
   );
 }

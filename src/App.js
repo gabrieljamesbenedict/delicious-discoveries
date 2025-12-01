1d28894248443ce0af8823dd20bc5b20522713cf
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-02 02:05:33 +0800

    feat: restructure Header component and update routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import Header from './Component/Header';
+import Header from './Components/Header';
 import './Styles/App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Pages/Home';
+import AboutUs from './Pages/AboutUs';
+import Delicacies from './Pages/Delicacies';
+import Vendors from './Pages/Vendors';
+import ContactUs from './Pages/ContactUs';
+
+
+const router = createBrowserRouter([
+  {path:"/home", element:<Home />},
+  {path:"/about", element:<AboutUs />},
+  {path:"/delicacies", element:<Delicacies />},
+  {path:"/vendors", element:<Vendors />},
+  {path:"/contact-us", element:<ContactUs />}
+]);
 
 function App() {
   return (
     <>
       <Header />
+      <RouterProvider router={router} />
     </>
   );
 }

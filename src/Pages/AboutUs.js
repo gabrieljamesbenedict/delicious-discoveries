981b0462ff6da207eaecb4234e65518023c27b25
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-02 21:48:58 +0800

    feat: implement About Us page with structured content and styles

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,8 +1,21 @@
+import "../Styles/AboutUs.css";
+
 function AboutUs() {
     return (
-        <>
-            <p>About Us Page</p>
-        </>
+        <div className="about-us-container">
+            <h1>About Delicious Discoveries</h1>
+            <p>
+                Put text here
+            </p>
+            <h2>Our Mission</h2>
+            <p>
+                Put text here
+            </p>
+            <h2>Culinary Heritage of Marinduque</h2>
+            <p>
+                Put text here
+            </p>
+        </div>
     );
 }
 

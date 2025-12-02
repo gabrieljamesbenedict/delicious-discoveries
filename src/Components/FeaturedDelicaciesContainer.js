4569fde1a92318506e2c4813f54e96788ff92817
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-02 19:00:39 +0800

    feat: add FeaturedDelicaciesContainer component to display featured delicacies

---
+++ src/Components/FeaturedDelicaciesContainer.js
@@ -0,0 +1,16 @@
+import "../Styles/FeaturedDelicacies.css";
+import FeaturedDelicacies from "./FeaturedDelicacies";
+
+import imageNotFound from "../Assets/image-not-found.png";
+
+function FeaturedDelicaciesContainer() {
+    return (
+        <div className="featured-delicacies-carousel">
+            <FeaturedDelicacies name="Lorem" description="Lorem ipsum dolor sit amet." imageURL={imageNotFound} />
+            <FeaturedDelicacies name="Lorem" description="Lorem ipsum dolor sit amet." imageURL={imageNotFound} />
+            <FeaturedDelicacies name="Lorem" description="Lorem ipsum dolor sit amet." imageURL={imageNotFound} />
+        </div>
+    );
+};
+
+export default FeaturedDelicaciesContainer;

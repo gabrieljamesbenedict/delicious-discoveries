c074a80fa00366977b840b94048e11df245af4ef
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-04 04:38:48 +0800

    refactor: update delicacy service functions to include parameters

--- src/Services/delicacyService.js
+++ src/Services/delicacyService.js
@@ -3,22 +3,29 @@
 const Delicacy = require("../Models/delicacyModel.js");
 const http = require('http');
 
+//return JSON.parse(result);
+
 export const getAllDelicacies = () => {
-
+    const result = http.get(`http://localhost:8080/api/delicacies`);
+    return result.data;
 };
 
-export const getDelicacy = () => {
-
+export const getDelicacy = (id) => {
+    const result = http.get(`http://localhost:8080/api/delicacies/${id}`);
+    return result.data;
 };
 
-export const postDelicacy = () => {
-
+export const postDelicacy = (data) => {
+    const result = http.post(`http://localhost:8080/api/delicacies`, data);
+    return result.data;
 };
 
-export const putDelicacy = () => {
-
+export const putDelicacy = (id, data) => {
+    const result = http.put(`http://localhost:8080/api/delicacies/${id}`, data);
+    return result.data;
 };
 
-export const deleteDelicacy = () => {
-
+export const deleteDelicacy = (id) => {
+    const result = http.delete(`http://localhost:8080/api/delicacies/${id}`);
+    return result.data;
 };

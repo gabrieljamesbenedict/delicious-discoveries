ecd318de575e65a1f2231e56a2cd55e7ec18002f
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-04 04:23:36 +0800

    feat: add imageURL field

--- server/model.js
+++ server/model.js
@@ -1,19 +1,21 @@
 export class delicacy {
-    constructor(name, description, ingredients, preparation, region) {
+    constructor(name, description, ingredients, preparation, region, imageURL) {
         this.name = name;
         this.description = description;
         this.ingredients = ingredients;
         this.preparation = preparation;
         this.region = region;
+        this.imageURL = imageURL;
     }
 }
 
 export class vendor {
-    constructor(name, specialty, contact, location) {
+    constructor(name, specialty, contact, location, imageURL) {
         this.name = name;
         this.specialty = specialty;
         this.contact = contact;
         this.location = location;
+        this.imageURL = imageURL;
     }
 }
 

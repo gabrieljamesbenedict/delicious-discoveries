0c7aac5859d95555a1f8cee5d4ef7734ee7499de
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-04 04:33:01 +0800

    feat: implement initial service functions for delicacies

---
+++ src/Services/delicacyService.js
@@ -0,0 +1,24 @@
+//import Delicacy from "../Models/delicacyModel.js";
+
+const Delicacy = require("../Models/delicacyModel.js");
+const http = require('http');
+
+export const getAllDelicacies = () => {
+
+};
+
+export const getDelicacy = () => {
+
+};
+
+export const postDelicacy = () => {
+
+};
+
+export const putDelicacy = () => {
+
+};
+
+export const deleteDelicacy = () => {
+
+};

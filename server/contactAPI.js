298f8917f95cfa6529f955527e6035771a2d2f3f
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-04 00:17:48 +0800

    feat: update postContact to use async/await for improved error handling

--- server/contactAPI.js
+++ server/contactAPI.js
@@ -1,3 +1,14 @@
-export const postContact = (req, res) => {
+export const postContact = async (req, res) => {
+    try {
+        const data = req.body;
+        const result = await run("messages", (coll) => coll.insertOne(data));
 
+        res.status(201).json({
+            message: "Message created",
+            id: result.insertedId
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 };

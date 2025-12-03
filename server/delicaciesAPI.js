e758035ca0462cc5e26834c0e9368b775001ace2
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-03 21:22:06 +0800

    feat: update delicaciesAPI.js to use async/await and improve error handling

--- server/delicaciesAPI.js
+++ server/delicaciesAPI.js
@@ -1,20 +1,89 @@
+import { run } from "./mongodb.js";
 
-export const getAllDelicacy = (req, res) => {
+export const getAllDelicacy = async (req, res) => {
+    try {
+        const delicacies = await run("delicacies", (coll) => coll.find().toArray());
 
+        res.status(200).json(delicacies);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 };
 
-export const getDelicacy = (req, res) => {
 
+export const getDelicacy = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const delicacy = await run("delicacies", (coll) => coll.findOne({ _id: new ObjectId(id) }));
+
+        if (!delicacy) {
+            return res.status(404).json({ error: "Delicacy not found" });
+        }
+
+        res.status(200).json(delicacy);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 };
 
-export const postDelicacy = (req, res) => {
 
+export const postDelicacy = async (req, res) => {
+    try {
+        const data = req.body;
+
+        const result = await run("delicacies", (coll) => coll.insertOne(data));
+
+        res.status(201).json({
+            message: "Delicacy created",
+            id: result.insertedId
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 };
 
-export const putDelicacy = (req, res) => {
 
+export const putDelicacy = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const update = req.body;
+
+        const result = await run("delicacies", (coll) =>
+            coll.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: update }
+            )
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ error: "Delicacy not found" });
+        }
+
+        res.status(200).json({ message: "Delicacy updated" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 };
 
-export const deleteDelicacy = (req, res) => {
-    
+
+export const deleteDelicacy = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const result = await run("delicacies", (coll) => coll.deleteOne({ _id: new ObjectId(id) }));
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: "Delicacy not found" });
+        }
+
+        res.status(200).json({ message: "Delicacy deleted" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 };

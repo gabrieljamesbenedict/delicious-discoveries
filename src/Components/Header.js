f4b03294615fc311b7b492fc46b043efa74e7711
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-04 12:50:33 +0800

    fix: update Home link in Header component to point to the correct route

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <div className="header-div">
             <h1 className="header-title"> Delicious Discoveries</h1>
             <ul className="header-nav">
-                <li><Link to={"/home"} className="header-nav-button" >Home</Link></li>
+                <li><Link to={"/"} className="header-nav-button" >Home</Link></li>
                 <li><Link to={"/about-us"} className="header-nav-button" >About</Link></li>
                 <li><Link to={"/delicacies"} className="header-nav-button" >Delicacies</Link></li>
                 <li><Link to={"/vendors"} className="header-nav-button" >Vendors</Link></li>

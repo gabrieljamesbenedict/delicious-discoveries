207dd74a4d48b6a850e16a1b792dc8fbd129650c
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-04 13:02:27 +0800

    feat: implement vendorService

--- src/Services/vendorService.js
+++ src/Services/vendorService.js
@@ -1,15 +1,15 @@
 export const getAllVendors = async () => {
-    const res = await fetch("http://localhost:8080/api/delicacies");
+    const res = await fetch("http://localhost:8080/api/vendors");
     return await res.json();
 };
 
 export const getVendor = async (id) => {
-    const res = await fetch(`http://localhost:8080/api/delicacies/${id}`);
+    const res = await fetch(`http://localhost:8080/api/vendors/${id}`);
     return await res.json();
 };
 
 export const postVendor = async (data) => {
-    const res = await fetch("http://localhost:8080/api/delicacies", {
+    const res = await fetch("http://localhost:8080/api/vendors", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -20,7 +20,7 @@ export const postVendor = async (data) => {
 };
 
 export const putVendor = async (id, data) => {
-    const res = await fetch(`http://localhost:8080/api/delicacies/${id}`, {
+    const res = await fetch(`http://localhost:8080/api/vendors/${id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
@@ -31,7 +31,7 @@ export const putVendor = async (id, data) => {
 };
 
 export const deleteVendor = async (id) => {
-    const res = await fetch(`http://localhost:8080/api/delicacies/${id}`, {
+    const res = await fetch(`http://localhost:8080/api/vendors/${id}`, {
         method: "DELETE"
     });
     return await res.json();

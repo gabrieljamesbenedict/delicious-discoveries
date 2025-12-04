e150c401076d4f68c5451e4ace233ad613cc73d2
Gabriel James Benedict <benok.loslos@gmail.com>
2025-12-04 12:50:14 +0800

    feat: update FeaturedDelicaciesContainer and FeaturedDelicacyCard with new delicacy details and improved layout

--- src/Components/FeaturedDelicaciesContainer.js
+++ src/Components/FeaturedDelicaciesContainer.js
@@ -6,9 +6,21 @@ import imageNotFound from "../Assets/image-not-found.png";
 function FeaturedDelicaciesContainer() {
     return (
         <div className="featured-delicacies-container">
-            <FeaturedDelicacies name="Lorem" description="Lorem ipsum dolor sit amet." imageURL={imageNotFound} />
-            <FeaturedDelicacies name="Lorem" description="Lorem ipsum dolor sit amet." imageURL={imageNotFound} />
-            <FeaturedDelicacies name="Lorem" description="Lorem ipsum dolor sit amet." imageURL={imageNotFound} />
+            <FeaturedDelicacies 
+                name="Kari-kari"    
+                description="One of Marinduque’s unique dishes is kari-kari, a spicy pork blood stew not to be confused with the more popular kare-kare, a totally different Filipino dish of oxtail and beef tripe in a savory peanut sauce. Similar to dinuguan or dugó-dugó but with less broth, traditional Marinduque kari-kari is made from pork blood and pork innards, like the heart, spleen, kidneys and small intestines. For picky eaters, the Boac version of this dish substitutes the innards with ground meat." 
+                imageURL={"images/delicacy/kari-kari.jpg"} 
+            />
+            <FeaturedDelicacies 
+                name="Ulang-ulang (Soup)" 
+                description="A sour soup of ulang (freshwater prawn) and shredded buko (coconut meat). As its name suggests, the ulang-ulang soup is traditionally made with ulang (freshwater prawns), but due to local scarcity hipon (saltwater shrimp) is often used as a substitute. The souring agent for this soup dish is calamansí, a citrus hybrid predominately cultivated in the Philippines and widely used in Filipino cuisine." 
+                imageURL={"images/delicacy/ulang-ulang.jpg"}
+            />
+            <FeaturedDelicacies 
+                name ="Uraro"
+                description="Perhaps the most well-known pasalubong from the island is uraró, a soft cookie made from arrowroot flour that originated from the Tagalog people of southern Luzon, particularly Laguna, Quezon and Marinduque. Arrowroot (Maranta arundinacea) is a root crop native to Central and South America, and was introduced to the Philippines through the Manila-Acapulco Galleon trade." 
+                imageURL={"images/delicacy/uraro.jpg"} 
+            />
         </div>
     );
 };
--- src/Components/FeaturedDelicacyCard.js
+++ src/Components/FeaturedDelicacyCard.js
@@ -1,20 +1,49 @@
 import "../Styles/Card.css";
+import imageNotFound from "../Assets/image-not-found.png";
+import { useState } from "react"; // needed
 
-function FeaturedDelicacyCard (data) {
-    const name = data.name;
-    const description = data.description;
-    const ingredients = data.ingredients;
-    const preparation = data.description;
-    const region = data.description;
-    const imageURL = data.imageURL;
+function FeaturedDelicacyCard (props) {
+    const delicacy = props.delicacy;
+    const name = delicacy.name;
+    const description = delicacy.description;
+    const ingredients = delicacy.ingredients;
+    const preparation = delicacy.preparation;
+    const region = delicacy.region;
+    const imageURL = delicacy.imageURL || imageNotFound;
+
+    const overview = props.overview;
+
+    const onClick = props.onClick;
+
+    const flexDir = overview? {flexDirection: 'column'} : {flexDirection: 'row'};
+
     return (
-        <div className="card">
-            <img src={imageURL} alt={name} />
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <p>{ingredients}</p>
-            <p>{preparation}</p>
-            <p>{region}</p>
+        <div className="card" style={flexDir} onClick={onClick}>
+            <div className="card-image">
+                <img src={imageURL} alt={imageURL} />
+            </div>
+
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+
+                <p className="card-description">{description}</p>
+
+                {!overview && (
+                    <>
+                        <div className="card-section">
+                            <h3>Ingredients</h3>
+                            <p>{ingredients}</p>
+                        </div>
+
+                        <div className="card-section">
+                            <h3>Preparation</h3>
+                            <p>{preparation}</p>
+                        </div>
+                    </>
+                )}
+
+                <span className="card-region-badge">{region}</span>
+            </div>
         </div>
     );
 }
